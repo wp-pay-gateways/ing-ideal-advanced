20383d100fa7f8f7a70d769c066b94f72599c7ac
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 13:31:52 +0100

    Extend from iDEAL Advanced library.

--- src/Integration.php
+++ src/Integration.php
@@ -1,22 +1,10 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_ING_IDealAdvanced_Integration {
+class Pronamic_WP_Pay_Gateways_ING_IDealAdvanced_Integration extends Pronamic_WP_Pay_Gateways_IDealAdvanced_AbstractIntegration {
 	public function __construct() {
 		$this->id         = 'ing-ideal-advanced';
 		$this->name       = 'ING - iDEAL Advanced';
 		$this->provider   = 'ing';
 		$this->deprecated = true;
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvanced_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvanced_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvanced_Gateway';
 	}
 }
